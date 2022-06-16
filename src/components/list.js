8072f9f53f4d4378b69d7ea1059a678180dcb3b3
Kalyani <kalyanichow8@gmail.com>
2022-06-16 11:12:47 +0530

    display list of students

--- src/components/list.js
+++ src/components/list.js
@@ -1,14 +1,14 @@
 import React from 'react'
 function List(props){
+    const nameList = []
     const stuList = props.list
-    stuList.forEach((ele) => {
-            return(
-                <div>
-                    <span>{ele.firstname}</span>
-                    <span>{ele.lastname}</span>
-                </div>
-            )
+       stuList.forEach((ele, i) => {
+            nameList.push(<span key={`${i} ${ele.firstname}`}>{`FirstName : ${ele.firstname}  LastName : ${ele.lastname} `}</span>)
         })
+              return(
+                <div>{nameList}</div>
+              )          
+     
 }
 
 export default List;
